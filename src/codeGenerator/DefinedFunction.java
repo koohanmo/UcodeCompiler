414421f9552e8b1a19275a8e07358717ace11514
KooHanmo <hanmo11045@gmail.com>
2016-06-08 17:50:41 +0900

    add FileWriter flush

--- src/codeGenerator/DefinedFunction.java
+++ src/codeGenerator/DefinedFunction.java
@@ -2,21 +2,18 @@ package codeGenerator;
 
 
 
-/*
- *  미리 정의 된 함수 선언
-*/
 public class DefinedFunction {
  
 	public DefinedFunction(){
 		
 	}
 	
-	//미리 정의된 함수를 uco파일에 선언
+
 	public void writeDefinedFunctions(){
-		
+			
 	}
 	
-	//미리 정의된 함수 정의
+
 	
 	
 	
